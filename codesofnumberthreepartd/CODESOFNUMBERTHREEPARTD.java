25ac595dfe27350aba7de5a51ac30d9114c3158d
MATWALE PHILLIP <matwalephillip14@gmail.com>
2025-10-19 18:40:37 +0300

    Add files via upload

---
+++ codesofnumberthreepartd/CODESOFNUMBERTHREEPARTD.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.codesofnumberthreepartd;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author OTHERS
+ */
+public class CODESOFNUMBERTHREEPARTD {
+
+    public static void main(String[] args) {
+          Scanner input = new Scanner(System.in);
+          int count = 0;
+          int[] gradeCount = new int[10];
+          while (count<5){
+        System.out.println("Enter Student's score");
+        int score = input.nextInt();
+        
+        int grade;
+        String remark;
+        if(score>=80 && score<=100){
+            grade = 1; remark = "Distinction";
+        }else if (score>=75){
+            grade = 2; remark = "Distinction";
+        }else if (score>=66){
+            grade = 3; remark = "Credit";
+        }else if (score>=60){
+            grade = 4; remark = "Credit";
+        }else if (score>=50){
+            grade = 5; remark = "Credit";
+        }else if (score>=45){
+            grade = 6; remark = "Credit";
+        }else if (score>=35){
+            grade = 7; remark = "Pass";
+        }else if (score>=30){
+            grade = 8; remark = "Pass";
+        }else{
+            grade = 9; remark = "Fail";
+        }
+        System.out.println("Score:"+ score);
+        System.out.println("Grade:"+ grade);
+        System.out.println("Remark:"+ remark);
+        System.out.println("---------------------------");
+        gradeCount[grade]++;
+        count++;
+                
+    }
+          System.out.println("\n===Grade Summary===");
+          for (int i = 1; i<=9; i++){
+              System.out.println("Grade" + i + ":" + gradeCount[i] + "student(s)");
+          }
+}
+
+       
+    
+}
